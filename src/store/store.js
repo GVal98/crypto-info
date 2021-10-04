64958ceee03e5443bd7486a9d41dea56afd59a33
GVal98 <GVal98@gmail.com>
2021-10-04 21:21:45 +0300

    Initial setup

---
+++ src/store/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { coinsApi } from '../api/coinsApi'
+
+const store = configureStore({
+  reducer: {
+    [coinsApi.reducerPath]: coinsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(coinsApi.middleware),
+})
+setupListeners(store.dispatch)
+
+export default store
