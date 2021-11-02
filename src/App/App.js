060fa1aad1b2bf0f1e4596dbd3a1bfe180212552
GVal98 <GVal98@gmail.com>
2021-11-02 23:00:11 +0300

    refactor: moved pages to separate folder

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import { Container } from '@material-ui/core'
 import Header from '../components/Header'
-import CoinsList from "../components/CoinsList"
-import CoinInfo from "../components/CoinInfo"
+import CoinsList from "../pages/CoinsList"
+import CoinInfo from "../pages/CoinInfo"
 
 export default function App() {
   return (
