aaed86fc2f46459b12ec0957520a5cd13ab83773
GVal98 <GVal98@gmail.com>
2021-11-04 05:24:31 +0300

    feat: coin extra info

--- src/components/CoinInfo/CoinInfo.js
+++ src/components/CoinInfo/CoinInfo.js
@@ -1,16 +1,52 @@
-import { useParams } from 'react-router-dom'
-import { Typography } from '@material-ui/core'
+import { useGetCoinInfoQuery } from '../../api/coinsApi'
+import { useLocation, useParams } from 'react-router-dom'
+import { Typography, Box, Container } from '@material-ui/core'
 import MainInfo from './MainInfo'
 import Chart from './Chart'
+import ExtraInfo from './ExtraInfo'
+import Loader from '../Loader'
 
 export default function CoinInfo() {
   const { coinId } = useParams()
+  const location = useLocation()
+  const cachedData = location.state
+  const { data, isError, isFetching } = useGetCoinInfoQuery(coinId)
+  
+  let formattedData = null
+  if (data) formattedData = { 
+    name: data.name,
+    image: data.image.large,
+    symbol: data.symbol,
+    price: data.market_data.current_price.usd,
+    change24h: data.market_data.price_change_percentage_24h,
+    marketCap: data.market_data.market_cap.usd,
+    marketCapRank: data.market_cap_rank,
+    category: data.categories[0],
+    homePage: data.links.homepage[0],
+    circulatingSupply: data.market_data.circulating_supply,
+    maxSupply: data.market_data.max_supply,
+    ath: data.market_data.ath.usd,
+    athDate: data.market_data.ath_date.usd,
+    athChange: data.market_data.ath_change_percentage.usd
+  }
 
   return (
     <>
-      <MainInfo coinId={coinId} />
-      <Typography variant="h5" component="h2">Annual price chart</Typography>
-      <Chart coinId={coinId} />
+      {isError && 'Error'}
+      <Loader loading={isFetching} />
+      <MainInfo data={data ? formattedData : cachedData} />
+      <Container maxWidth="lg" sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Box flexGrow="1" maxWidth="65%">
+          <Typography variant="h5" component="h2">Annual price chart</Typography>
+          <Chart coinId={coinId} />
+        </Box>
+        <Box flexShrink="0" minWidth="30%">
+          <Typography variant="h5" component="h2">Info</Typography>
+          {isError && 'Error'}
+          <Loader loading={isFetching} />
+          {data && <ExtraInfo data={formattedData} />}
+        </Box>
+      </Container>
     </>
   )
 }
---
+++ src/components/CoinInfo/ExtraInfo/ExtraInfo.js
@@ -0,0 +1,32 @@
+import { TableContainer, Table, Paper, TableBody, Link } from '@material-ui/core'
+import Row from './Row'
+
+const formatDate = dateString => {
+  const date = new Date(dateString)
+  return date.toLocaleDateString()
+}
+
+const formatSypply = (supply, symbol) => {
+  return `${Math.round(supply) || 'Infinity'} ${symbol}`
+}
+
+export default function ExtraInfo({ data }) {
+  const symbol = data.symbol.toUpperCase()
+  const homePageLink = <Link href={data.homePage}>{data.homePage}</Link>
+  
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableBody>
+          <Row name="Category" value={data.category} />
+          <Row name="Home Page" value={homePageLink} />
+          <Row name="Circulating Supply" value={formatSypply(data.circulatingSupply, symbol)} />
+          <Row name="Max Supply" value={formatSypply(data.maxSupply, symbol)} />
+          <Row name="All Time High" value={`${data.ath} ${symbol}`} />
+          <Row name="ATH Date" value={formatDate(data.athDate)} />
+          <Row name="ATH Change" value={`${data.athChange.toFixed(2)}%`} />
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
