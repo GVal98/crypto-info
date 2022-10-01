b0849e716ad272bdc92a4929a66c70e0fe789d14
GVal98 <GVal98@gmail.com>
2022-10-01 18:49:54 +0300

    feat: added login input hints

--- src/components/LoginModal/LoginForm/LoginFormView.jsx
+++ src/components/LoginModal/LoginForm/LoginFormView.jsx
@@ -16,8 +16,8 @@ export default function LoginFormView({ isSubmitting, errors, touched, handleClo
   return (
     <Form>
       <DialogContent>
-        <Field type="text" name="username" label="Username" autoFocus error={Boolean(touched.username && errors.username)} helperText={touched.username && errors.username} as={MaterialField} />
-        <Field type="password" name="password" label="Password" error={Boolean(touched.password && errors.password)} helperText={touched.password && errors.password} as={MaterialField} />
+        <Field type="text" name="username" label="Username (Anything)" autoFocus error={Boolean(touched.username && errors.username)} helperText={touched.username && errors.username} as={MaterialField} />
+        <Field type="password" name="password" label="Password (Anything)" error={Boolean(touched.password && errors.password)} helperText={touched.password && errors.password} as={MaterialField} />
         <Loader loading={isSubmitting} />
       </DialogContent>
       <DialogActions>
